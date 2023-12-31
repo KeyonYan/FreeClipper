fc24f81419f91f95acf438697cd16808588cbf9c
Shadowsight9 <huangzheng@shu.edu.cn>
2023-12-31 16:54:36 +0800

    feat: add tailwindcss support

--- packages/core/src/dom-inspector.ts
+++ packages/core/src/dom-inspector.ts
@@ -1,6 +1,7 @@
-import { LitElement, css, html } from 'lit'
+import { LitElement, css, html, unsafeCSS } from 'lit'
 import { property, query, state } from 'lit/decorators.js'
 import { styleMap } from 'lit/directives/style-map.js'
+import tailwindInjectedCss from './tailwind.out.css?raw'
 
 const styleId = '__dom-inspector-unique-id'
 
@@ -442,204 +443,111 @@ export class DomInspectElement extends LitElement {
         id="inspector-switch"
         class="inspector-switch ${this.open
           ? 'active-inspector-switch'
-          : ''} ${this.moved ? 'move-inspector-switch' : ''}"
+          : ''} ${this.moved ? 'move-inspector-switch' : ''} bg-slate-300 border"
         style=${styleMap({ display: this.showSwitch ? 'flex' : 'none' })}
       >
-        ${this.open
-          ? html`
-              <svg
-                t="1677801709811"
-                class="icon"
-                viewBox="0 0 1024 1024"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                p-id="1110"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                width="1em"
-                height="1em"
-              >
-                <path
-                  d="M546.56 704H128c-19.2 0-32-12.8-32-32V256h704v194.56c10.928 1.552 21.648 3.76 32 6.832V128c0-35.2-28.8-64-64-64H128C92.8 64 64 92.8 64 128v544c0 35.2 28.8 64 64 64h425.392a221.936 221.936 0 0 1-6.848-32zM96 128c0-19.2 12.8-32 32-32h640c19.2 0 32 12.8 32 32v96H96V128z"
-                  fill="#34495E"
-                  p-id="1111"
-                ></path>
-                <path
-                  d="M416 160m-32 0a32 32 0 1 0 64 0 32 32 0 1 0-64 0Z"
-                  fill="#00B42A"
-                  p-id="1112"
-                ></path>
-                <path
-                  d="M288 160m-32 0a32 32 0 1 0 64 0 32 32 0 1 0-64 0Z"
-                  fill="#F7BA1E"
-                  p-id="1113"
-                ></path>
-                <path
-                  d="M160 160m-32 0a32 32 0 1 0 64 0 32 32 0 1 0-64 0Z"
-                  fill="#F53F3F"
-                  p-id="1114"
-                ></path>
-                <path
-                  d="M382.848 658.928l99.376-370.88 30.912 8.272-99.36 370.88zM318.368 319.2L160 477.6l158.4 158.4 22.64-22.624-135.792-135.776 135.776-135.776zM768 480c-13.088 0-25.888 1.344-38.24 3.84l6.24-6.24-158.4-158.4-22.64 22.624 135.792 135.776-135.776 135.776 22.656 22.624 2.208-2.224a190.768 190.768 0 0 0 30.928 148.08l-116.672 116.656c-10.24 10.24-10.24 26.896 0 37.136l27.76 27.76c5.12 5.12 11.84 7.68 18.56 7.68s13.456-2.56 18.56-7.68l120.992-120.96A190.56 190.56 0 0 0 768 864c105.872 0 192-86.128 192-192s-86.128-192-192-192z m-159.12 193.136c0-88.224 71.776-160 160-160 10.656 0 21.04 1.152 31.12 3.152V672c0 19.2-12.8 32-32 32h-156a160.144 160.144 0 0 1-3.12-30.864z m-68.464 263.584l-19.632-19.632 110.336-110.336c6.464 6.656 13.392 12.848 20.752 18.528l-111.456 111.44z m228.464-103.584c-65.92 0-122.576-40.096-147.056-97.136H768c35.2 0 64-28.8 64-64v-145.776c56.896 24.544 96.88 81.12 96.88 146.912 0 88.224-71.776 160-160 160z"
-                  fill="#006AFF"
-                  p-id="1115"
-                ></path>
-                <path
-                  d="M864.576 672c0 52.928-43.072 96-96 96v32a128 128 0 0 0 128-128h-32z"
-                  fill="#34495E"
-                  p-id="1116"
-                ></path>
-              </svg>
-            `
-          : html`<svg
-              t="1677801709811"
-              class="icon"
-              viewBox="0 0 1024 1024"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              p-id="1110"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              width="1em"
-              height="1em"
-            >
-              <path
-                d="M546.56 704H128c-19.2 0-32-12.8-32-32V256h704v194.56c10.928 1.552 21.648 3.76 32 6.832V128c0-35.2-28.8-64-64-64H128C92.8 64 64 92.8 64 128v544c0 35.2 28.8 64 64 64h425.392a221.936 221.936 0 0 1-6.848-32zM96 128c0-19.2 12.8-32 32-32h640c19.2 0 32 12.8 32 32v96H96V128z"
-                fill="currentColor"
-                p-id="1111"
-              ></path>
-              <path
-                d="M416 160m-32 0a32 32 0 1 0 64 0 32 32 0 1 0-64 0Z"
-                fill="currentColor"
-                p-id="1112"
-              ></path>
-              <path
-                d="M288 160m-32 0a32 32 0 1 0 64 0 32 32 0 1 0-64 0Z"
-                fill="currentColor"
-                p-id="1113"
-              ></path>
-              <path
-                d="M160 160m-32 0a32 32 0 1 0 64 0 32 32 0 1 0-64 0Z"
-                fill="currentColor"
-                p-id="1114"
-              ></path>
-              <path
-                d="M382.848 658.928l99.376-370.88 30.912 8.272-99.36 370.88zM318.368 319.2L160 477.6l158.4 158.4 22.64-22.624-135.792-135.776 135.776-135.776zM768 480c-13.088 0-25.888 1.344-38.24 3.84l6.24-6.24-158.4-158.4-22.64 22.624 135.792 135.776-135.776 135.776 22.656 22.624 2.208-2.224a190.768 190.768 0 0 0 30.928 148.08l-116.672 116.656c-10.24 10.24-10.24 26.896 0 37.136l27.76 27.76c5.12 5.12 11.84 7.68 18.56 7.68s13.456-2.56 18.56-7.68l120.992-120.96A190.56 190.56 0 0 0 768 864c105.872 0 192-86.128 192-192s-86.128-192-192-192z m-159.12 193.136c0-88.224 71.776-160 160-160 10.656 0 21.04 1.152 31.12 3.152V672c0 19.2-12.8 32-32 32h-156a160.144 160.144 0 0 1-3.12-30.864z m-68.464 263.584l-19.632-19.632 110.336-110.336c6.464 6.656 13.392 12.848 20.752 18.528l-111.456 111.44z m228.464-103.584c-65.92 0-122.576-40.096-147.056-97.136H768c35.2 0 64-28.8 64-64v-145.776c56.896 24.544 96.88 81.12 96.88 146.912 0 88.224-71.776 160-160 160z"
-                fill="currentColor"
-                p-id="1115"
-              ></path>
-              <path
-                d="M864.576 672c0 52.928-43.072 96-96 96v32a128 128 0 0 0 128-128h-32z"
-                fill="currentColor"
-                p-id="1116"
-              ></path>
-            </svg>`}
+        ${this.open ? 'close' : 'open'}
+
       </div>
     `
   }
 
-  static styles = css`
-    .dom-inspector-container {
-      position: fixed;
-      pointer-events: none;
-      z-index: 999999;
-      font-family: 'PingFang SC';
-      .margin-overlay {
+  static userStyles = css`
+  .dom-inspector-container {
+    position: fixed;
+    pointer-events: none;
+    z-index: 999999;
+    font-family: 'PingFang SC';
+    .margin-overlay {
+      position: absolute;
+      inset: 0;
+      border-style: solid;
+      border-color: rgba(255, 155, 0, 0.3);
+      .border-overlay {
         position: absolute;
         inset: 0;
         border-style: solid;
-        border-color: rgba(255, 155, 0, 0.3);
-        .border-overlay {
+        border-color: rgba(255, 200, 50, 0.3);
+        .padding-overlay {
           position: absolute;
           inset: 0;
           border-style: solid;
-          border-color: rgba(255, 200, 50, 0.3);
-          .padding-overlay {
+          border-color: rgba(77, 200, 0, 0.3);
+          .content-overlay {
             position: absolute;
             inset: 0;
-            border-style: solid;
-            border-color: rgba(77, 200, 0, 0.3);
-            .content-overlay {
-              position: absolute;
-              inset: 0;
-              background: rgba(120, 170, 210, 0.7);
-            }
+            background: rgba(120, 170, 210, 0.7);
           }
         }
       }
     }
-    .element-info {
-      position: absolute;
-    }
-    .element-info-content {
-      max-width: 100%;
-      font-size: 12px;
-      color: #000;
-      background-color: #fff;
-      word-break: break-all;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
-      box-sizing: border-box;
-      padding: 4px 8px;
-      border-radius: 4px;
-    }
-    .element-info-top {
-      top: -4px;
-      transform: translateY(-100%);
-    }
-    .element-info-bottom {
-      top: calc(100% + 4px);
-    }
-    .element-info-top-inner {
-      top: 4px;
-    }
-    .element-info-bottom-inner {
-      bottom: 4px;
-    }
-    .element-info-left {
-      left: 0;
-      display: flex;
-      justify-content: flex-start;
-    }
-    .element-info-right {
-      right: 0;
-      display: flex;
-      justify-content: flex-end;
-    }
-    .element-name .element-title {
-      color: coral;
-      font-weight: bold;
-    }
-    .element-name .element-tip {
-      color: #006aff;
-    }
-    .path-line {
-      color: #333;
-      line-height: 12px;
-      margin-top: 4px;
-    }
-    .inspector-switch {
-      position: fixed;
-      z-index: 9999999;
-      top: 16px;
-      left: 50%;
-      font-size: 22px;
-      transform: translateX(-50%);
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      background-color: rgba(255, 255, 255, 0.8);
-      color: #555;
-      height: 32px;
-      width: 32px;
-      border-radius: 50%;
-      box-shadow: 0px 1px 2px -2px rgba(0, 0, 0, 0.2),
-        0px 3px 6px 0px rgba(0, 0, 0, 0.16),
-        0px 5px 12px 4px rgba(0, 0, 0, 0.12);
-      cursor: pointer;
-    }
-    .active-inspector-switch {
-      color: #006aff;
-    }
-    .move-inspector-switch {
-      cursor: move;
-    }
-  `
+  }
+  .element-info {
+    position: absolute;
+  }
+  .element-info-content {
+    max-width: 100%;
+    font-size: 12px;
+    color: #000;
+    background-color: #fff;
+    word-break: break-all;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+    box-sizing: border-box;
+    padding: 4px 8px;
+    border-radius: 4px;
+  }
+  .element-info-top {
+    top: -4px;
+    transform: translateY(-100%);
+  }
+  .element-info-bottom {
+    top: calc(100% + 4px);
+  }
+  .element-info-top-inner {
+    top: 4px;
+  }
+  .element-info-bottom-inner {
+    bottom: 4px;
+  }
+  .element-info-left {
+    left: 0;
+    display: flex;
+    justify-content: flex-start;
+  }
+  .element-info-right {
+    right: 0;
+    display: flex;
+    justify-content: flex-end;
+  }
+  .element-name .element-title {
+    color: coral;
+    font-weight: bold;
+  }
+  .element-name .element-tip {
+    color: #006aff;
+  }
+  .path-line {
+    color: #333;
+    line-height: 12px;
+    margin-top: 4px;
+  }
+  .inspector-switch {
+    position: fixed;
+    z-index: 9999999;
+    top: 16px;
+    left: 50%;
+    font-size: 22px;
+    transform: translateX(-50%);
+   
+    cursor: pointer;
+  }
+  .active-inspector-switch {
+    color: #006aff;
+  }
+  .move-inspector-switch {
+    cursor: move;
+  }
+`
+
+  static styles = [this.userStyles, unsafeCSS(tailwindInjectedCss)]
 }
