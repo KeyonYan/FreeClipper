0bf235752bbd477673461078cee5512e0c36fea1
KeyonYan <835254227@qq.com>
2024-01-09 16:54:18 +0800

    chore: lit 3.1

--- packages/core/src/notion-fetch.ts
+++ packages/core/src/notion-fetch.ts
@@ -1,19 +1,19 @@
 import { Client } from '@notionhq/client'
 
-const auth = ''
-const pageId = ''
-
+const auth = '' // TODO
+const pageId = '' // TODO
+const LIMIT_BLOCK_COUNT = 50
 export async function uploadToNotion(blocks: any[]) {
   const notion = new Client({
-    auth: auth,
+    auth,
     fetch: (url: string, init) => {
       return fetch(url.replace('https://api.notion.com/v1', 'http://localhost:5173/notionapi'), init)
-    }
+    },
   })
-  for (let i = 0; i < blocks.length; i+=100) {
+  for (let i = 0; i < blocks.length; i += LIMIT_BLOCK_COUNT) {
     const newHeadingResponse = await notion.blocks.children.append({
       block_id: pageId,
-      children: blocks.slice(i, i + 100)
+      children: blocks.slice(i, i + LIMIT_BLOCK_COUNT),
     })
   }
 }
