67d7a0f65cd35bb08d30708d118ad264fadb9088
KeyonYan <835254227@qq.com>
2024-01-13 18:58:32 +0800

    style: Open Button

--- packages/core/src/toast.ts
+++ packages/core/src/toast.ts
@@ -21,11 +21,12 @@ export class Toaster extends LitElement {
         ${this.toasts.map(
           ({ message, url }) =>
             html`
-            <div class=" hover:shadow-lg transition-all duration-400 items-center justify-center p-2 flex flex-row gap-2 rounded-lg shadow-md bg-white text-black">
+            <div class="hover:shadow-lg transition-all duration-400 items-center justify-center p-2 flex flex-row gap-2 rounded-lg shadow-md bg-white text-black">
               <div>${message}</div>
               ${url && html`
-                <a href=${url}>
+                <a href=${url} class='inline-flex items-center gap-2 justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-[#F4F4F5] hover:text-accent-foreground h-9 px-2 py-1'>
                   <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.5 1C4.22386 1 4 1.22386 4 1.5C4 1.77614 4.22386 2 4.5 2H12V13H4.5C4.22386 13 4 13.2239 4 13.5C4 13.7761 4.22386 14 4.5 14H12C12.5523 14 13 13.5523 13 13V2C13 1.44772 12.5523 1 12 1H4.5ZM6.60355 4.89645C6.40829 4.70118 6.09171 4.70118 5.89645 4.89645C5.70118 5.09171 5.70118 5.40829 5.89645 5.60355L7.29289 7H0.5C0.223858 7 0 7.22386 0 7.5C0 7.77614 0.223858 8 0.5 8H7.29289L5.89645 9.39645C5.70118 9.59171 5.70118 9.90829 5.89645 10.1036C6.09171 10.2988 6.40829 10.2988 6.60355 10.1036L8.85355 7.85355C9.04882 7.65829 9.04882 7.34171 8.85355 7.14645L6.60355 4.89645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>  
+                  Open
                 </a>
                 `
               }
